b158209e33d560369fe2ba6e53ad155f6245562a
Fonzie! <twistfons@live.nl>
2019-01-26 11:40:34 +0100

    Player moves to mouse

---
+++ cozyhome/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float movementSpeed = 5;
+
+    // where to move to
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // only follow set the targetPosition when the user clicks
+        if (Input.GetMouseButtonDown(0))
+        {
+            // set targetPosition to the mouseposition relative to the screen
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // player must not move in 3D!!
+            targetPosition.z = 0;
+        }
+
+        if (transform.position != targetPosition)
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+    }
+}
