822acca1d1fd880a947876f0af3d0bff8ab3e806
Mark de Haan <happytaoist@gmail.com>
2019-01-26 15:15:10 +0100

    ai added

---
+++ cozyhome/Assets/Scripts/AIScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIScript : MonoBehaviour
+{
+    float[] direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new float[2];
+        
+    }
+    void randomizeDirection()
+    {
+        direction[0] = Random.Range(-10f, 10f);
+        direction[1] = Random.Range(-10f, 10f);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        {
+            randomizeDirection();
+            
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction[0], direction[1]);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
