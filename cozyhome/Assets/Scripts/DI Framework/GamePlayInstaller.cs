4ab72ae1db245214aa9a42241fafb511d409d700
Erik de Roos <xorgye@gmail.com>
2019-01-27 00:24:14 +0100

    Basic DI injection for scenes

---
+++ cozyhome/Assets/Scripts/DI Framework/GamePlayInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GamePlayInstaller : MonoInstaller<RootInstaller>
+{
+    public override void InstallBindings()
+    {
+
+    }
+}
---
+++ cozyhome/Assets/Scripts/DI Framework/RootInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class RootInstaller : MonoInstaller<RootInstaller>
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
