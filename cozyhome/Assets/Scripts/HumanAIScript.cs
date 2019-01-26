74cab43e98c9213aae0c87ff9603d1b7d31b847b
Erik de Roos <xorgye@gmail.com>
2019-01-26 17:58:01 +0100

    control monster or human

---
+++ cozyhome/Assets/Scripts/HumanAIScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanAIScript : MonoBehaviour
+{
+    float[] direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new float[2];
+        
+    }
+    void randomizeDirection()
+    {
+        direction[0] = Random.Range(-10f, 10f);
+        direction[1] = Random.Range(-10f, 10f);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        {
+            randomizeDirection();
+            
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction[0], direction[1]);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameManagerScript gmc = GameObject.Find("GameManagerController").GetComponent<GameManagerScript>();
+            gmc.OnMonsterGet();
+        }
+    }
+}
