77601d6b3398644c1c73b54d629965e78970c7f6
Fonzie! <twistfons@live.nl>
2019-01-26 17:30:17 +0100

    UI for actions

---
+++ cozyhome/Assets/Scripts/PerformAction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerformAction : MonoBehaviour
+{
+    public Transform ownerObject;
+    public GameObject resolutionPrefab;
+    public float coolDown;
+    public Color coolDownColor;
+
+    private float currentCoolDown = 0;
+    
+    void Update()
+    {
+        if (currentCoolDown > Time.time)
+            GetComponent<Image>().color = coolDownColor;
+        else
+            GetComponent<Image>().color = Color.white;
+    }
+
+    public void DoTheThing()
+    {
+        print("Doing the thing!");
+
+        if (Time.time < currentCoolDown)
+        {
+            Instantiate(resolutionPrefab, ownerObject.position, ownerObject.rotation);
+            currentCoolDown = Time.time + coolDown;
+        }
+    }
+}
