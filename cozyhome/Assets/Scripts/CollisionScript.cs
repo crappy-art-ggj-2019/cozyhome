ad83963eecaffc540febaf87e73e6678c1489367
Fonzie! <twistfons@live.nl>
2019-01-26 15:17:04 +0100

    Specifically ignored collision implemented

--- cozyhome/Assets/Scripts/CollisionScript.cs
+++ cozyhome/Assets/Scripts/CollisionScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
-    public string[] blocks;
+    public List<string> blocks;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,16 @@ public class CollisionScript : MonoBehaviour
     {
         foreach (string block in blocks)
         {
+            // if not supposed to block
+            if (!blocks.Contains(collision.gameObject.tag))
+            {
+                // ignore this specific collision
+                Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            }
+
+            // if it's a player and supposed to block
             if (block == "Player" && collision.gameObject.CompareTag(block))
+                // call its "StopMoving" function to make it stop moving
                 collision.gameObject.GetComponent<PlayerBehaviour>().StopMoving();
         }
     }
