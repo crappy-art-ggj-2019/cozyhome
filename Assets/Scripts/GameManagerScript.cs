65e54538f1101e9da9d448f6a1c03c78e232c51f
Mark de Haan <happytaoist@gmail.com>
2019-01-26 01:06:02 +0100

    Most basic gamelayout setup

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField] enum GameState { MainMenu, Preloader, Selection, GameCycle, Pause, GameOver }
+
+    private void Awake()
+    {
+        SetupSingleton();
+
+    }
+    private void SetupSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
