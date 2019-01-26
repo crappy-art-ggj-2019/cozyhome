8d8e3343c54327cd9224beab214a16c3df86ec23
Fonzie! <twistfons@live.nl>
2019-01-26 19:07:06 +0100

    Added second human action

---
+++ cozyhome/Assets/Scripts/TrapBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBehaviour : MonoBehaviour
+{
+    public List<string> targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (targets.Contains(collision.gameObject.tag))
+        {
+            print("KILL! MUST KILL!");
+
+            collision.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
