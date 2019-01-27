97f20473974488d0b604b428245ab218573a1d44
Erik de Roos <xorgye@gmail.com>
2019-01-27 11:00:23 +0100

    Recoupling ending to DI system

---
+++ cozyhome/Assets/EndingPanelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class EndingPanelScript : MonoBehaviour
+{
+    [Inject]
+    public SceneLoaderScript SceneLoader;
+
+    public void Continue()
+    {
+        SceneLoader.LoadMenuScene();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
